00bcec5644dc21682e90c8b1210400540d3b810b
Cristian Espes <christian_zgza@hotmail.com>
2020-05-03 19:52:15 +0200

    Updated design.

--- InformateEnCasa/Presentation/Emergency Call/EmergencyCallView.swift
+++ InformateEnCasa/Presentation/Emergency Call/EmergencyCallView.swift
@@ -9,28 +9,34 @@
 import SwiftUI
 
 struct EmergencyCallView: View {
-    private let margin = (UIScreen.main.bounds.width / 2) - 110 - 8
+    
     @ObservedObject private var viewModel = EmergencyCallViewModel()
     
+    @State private var activePageIndex: Int = 0
+    
     var body: some View {
-        ScrollView {
-            VStack(spacing: 30) {
-                Text("Teléfono de información según la comunidad autónoma")
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                
-                Text("Seleccione la bandera para mostrar el teléfono de información")
-                    .font(.body)
-                    .foregroundColor(Color.gray)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(viewModel.regions, id: \.self) { region in
-                            VStack(spacing: 20) {
+        VStack(spacing: 20) {
+            Text("Tel. de información")
+                .font(.system(.largeTitle, design: .rounded))
+                .bold()
+                .padding(.horizontal)
+            
+            Text("Seleccione la bandera para mostrar el teléfono de información")
+                .font(.system(.subheadline, design: .rounded))
+                .foregroundColor(Color.gray)
+                .multilineTextAlignment(.center)
+                .lineLimit(nil)
+                .padding(.horizontal)
+            
+            GeometryReader { geometry in
+                PagingScrollView(
+                    activePageIndex: self.$activePageIndex,
+                    itemCount: self.viewModel.regions.count,
+                    pageWidth: geometry.size.width,
+                    tileWidth: CardCCAAView.width + 20,
+                    tilePadding: 20) {
+                        ForEach(self.viewModel.regions, id: \.self) { region in
+                            VStack(spacing: 10) {
                                 CardCCAAView(region: region)
                                 .onTapGesture {
                                     self.openPhone(with: region)
@@ -41,29 +47,29 @@ struct EmergencyCallView: View {
                                     .bold()
                             }
                         }
-                    }
-                    .padding(.horizontal, self.margin)
+                        
                 }
-                
-                HStack(spacing: 0) {
-                    Text("#quédate")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.primaryBlue)
-                    Text("encasa")
-                        .font(.title)
-                        .bold()
-                }
-                .padding(.horizontal)
-                
-                Text("En caso de urgencia sanitaria llamar al 112")
-                    .font(.body)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
             }
-            .padding(.vertical)
+            .frame(height: 220)
+            
+            HStack(spacing: 0) {
+                Text("#quédate")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.primaryBlue)
+                Text("encasa")
+                    .font(.title)
+                    .bold()
+            }
+            .padding(.horizontal)
+            
+            Text("En caso de urgencia sanitaria llamar al 112")
+                .font(.system(.body, design: .rounded))
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
         }
+        .padding(.vertical, 20)
     }
     
     private func openPhone(with region: Region) {
